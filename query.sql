46394696aa68c0fc8e9cac9f6175b728a6f582c3
marjmekk <marjmekk1@gmail.com>
2023-02-06 10:52:31 -0800

    V50K

--- query.sql
+++ query.sql
@@ -1,2 +1,12 @@
 -- kind a long list, but here's what manufactures make
 SELECT DISTINCT make, model FROM registration;
+
+-- which counties have the most electric vehicles?
+SELECT make, model, 
+Where county
+
+-- what are the values for electrification in this DB?
+
+-- things we care about:
+-- country
+-- electrification 
--- reset.bat
+++ reset.bat
@@ -1,3 +1,3 @@
-dropbd v50k
-createdb v50k
-psql -d v50k -f create.sql
+dropdb test
+createdb test
+psql -d test -f create-db.sql
